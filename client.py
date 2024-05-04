0d76f9d7c5416d88afc9d6335f01d7cc330183b8
ap <arsenicpaper9@gmail.com>
2024-05-04 23:29:13 +0300

    test commit

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket, threading
+
+
+serverIp = input()
+serverPort = int(input())
+
+
+def messageRecieve(sock):
+    while True:
+        try:
+            foreignMessage = sock.recv(1024).decode('utf-8')
+            if foreignMessage:
+                print(foreignMessage)
+        except:
+            print("SYSTEM: You have been disconnected from the server.")
+            print()
+            sock.close()
+            break
+
+def messageSend(sock):
+    while True:
+        message = input('')
+        sock.send(message.encode('utf-8'))
+
+
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mySocket.connect((serverIp, serverPort))
+
+threadRecv = threading.Thread(target=messageRecieve, args=(mySocket,))
+threadRecv.start()
+
+threadSend = threading.Thread(target=messageSend, args=(mySocket,))
+threadSend.start()
--- server.py
+++ server.py
@@ -1,21 +1,70 @@
-import socket
+import socket, threading
 
 
 serverPort = 9090  # SERVER PORT
-serverName = "server"  # SERVER NAME
 clientsAmount = 5  # NUMBER OF CLIENTS
 
-def serverStart(serverPort, serverName, clientsAmount):
+clientsConnections = list()
+clientsNames = dict()
+
+
+def clientHandler(conn, addr):
+    print(f"NEW CONNECTION: {addr}")
+    conn.send("SYSTEM: Please input your name:".encode("utf-8"))
+    clientName = conn.recv(1024).decode("utf-8")
+    clientsConnections.append(conn)
+    clientsNames[conn] = clientName
+    connected = True
+
+    broadcast(conn, f"User {clientName} has joined chat.")
+    print(f"{clientName} HAS CONNECTED FROM {addr}")
+    print()
+    while connected:
+        try:
+            clientMessage = conn.recv(1024).decode("utf-8")
+            if clientMessage:
+                print(f"{clientName}: {clientMessage}")
+                broadcast(conn, f"{clientName}: {clientMessage}")
+        except:
+            clientsConnections.remove(conn)
+            broadcast(conn, f"User {clientName} has left the chat.")
+            print(f"{clientName} FROM {addr} HAS DISCONNECTED")
+            print()
+            conn.close()
+            break
+
+def broadcast(conn, msg):
+    for clientConnection in clientsConnections:
+        if clientConnection != conn:
+            try:
+                clientConnection.send(msg.encode("utf-8"))
+            except:
+                clientConnection.close()
+                clientsConnections.remove(clientConnection)
+                del clientsNames[clientConnection]
+
+
+def serverStart(serverPort, clientsAmount):
     serverIp = socket.gethostbyname(socket.gethostname())
     
     print("CLIENTS NEED TO CONNECT TO THIS IP:")
     print("---------------")
     print(serverIp)
     print("---------------")
+    print()
 
-    mySocket = socket.socket()
-    mySocket.bind((serverIp, serverPort))
+    serverSocket = socket.socket()
+    serverSocket.bind((serverIp, serverPort))
+    serverSocket.listen(clientsAmount)
+    print("SERVER IS LISTENING FOR CONNECTIONS")
+    print()
 
-    mySocket.listen(clientsAmount)
+    while True:
+        conn, addr = serverSocket.accept()
+        thread = threading.Thread(target=clientHandler, args=(conn, addr))
+        thread.start()
+        print(f"ACTIVE CONNECTIONS: {threading.activeCount() - 1}")
+        print()
 
-serverStart(serverPort, serverName)
+
+serverStart(serverPort, clientsAmount)
